92b5fd899a7208c6058713ed605416f4f9f55f31
wangyonglei <549749526@qq.com>
2016-07-25 19:26:25 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/launch.js
+++ js/launch.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
+		// 球场列表
 	$('.qiuchang').click(function() {
 		$('.qiuchang_maxk').css('display', 'block');
 		$('.qiuchang_mask').css('display', 'block');
-		// 加载列表
 		$.ajax({
 			url: 'http://v.jiaolianpai.com/user/Venue/getList',
 			dataType: 'json',
@@ -12,7 +12,7 @@ $(document).ready(function() {
 					var listdata = data.data;
 					var html = "";
 					for (var i = 0; i < listdata.length; i++) {
-						html += '<li>' + listdata[i].title + '</li>';
+						html += '<li data-qid="' + listdata[i].id + '">' + listdata[i].title + '</li>';
 					};
 					$("#ball_list").html(html);
 				}
@@ -21,22 +21,83 @@ $(document).ready(function() {
 	})
 	$('#ball_list').on('click', 'li', function() {
 		$('.qiuchang_input').val('')
+		$('.qiuchang_inputid').html('')
 		var venue = $(this).html();
-		$('.qiuchang_input').val(venue)
+		var venueid = $(this).attr('data-qid');
+		$('.qiuchang_input').val(venue);
+		$('.qiuchang_inputid').html(venueid);
 		$('.qiuchang_maxk').css('display', 'none');
 		$('.qiuchang_mask').css('display', 'none');
 	})
 	$('.qiuchang_mask').click(function() {
-		$('.qiuchang_maxk').css('display', 'none');
-		$('.qiuchang_mask').css('display', 'none');
+			$('.qiuchang_maxk').css('display', 'none');
+			$('.qiuchang_mask').css('display', 'none');
+		})
+		// 球场列表搜索
+	var search = function() {
+		var title = $('#search_input').val();
+		$.ajax({
+			url: 'http://v.jiaolianpai.com/user/Venue/getList?title=' + title,
+			type: 'GET',
+			success: function(data) {
+				var data = jQuery.parseJSON(data);
+				var searchlist = data.data;
+				var html = "";
+				for (var i = 0; i < searchlist.length; i++) {
+					html += '<li>' + searchlist[i].title + '</li>';
+				};
+				$("#ball_list").html(html);
+			}
+		});
+	}
+	$('#search_btn').click(function() {
+		search();
 	})
+	$('#search_input').keyup(function() {
+		search();
+	})
+	$('#x').click(function() {
+			$('#search_input').val('');
+		})
+		// 邀请好友
+	$('.yaoqing').click(function() {
+		$('.yaoqing_maxk').css('display', 'block');
+		$('.yaoqing_yq').html('')
+	})
+	$('.yaoqing_btn').click(function(event) {
+
+		var rname = $('.yaoqing_name').val();
+		var mobile = $('.yaoqing_tel').val();
+		$('.yaoqing_yq').append(rname);
+		var yaoqing_namrel = {
+			rname: $('.yaoqing_name').val(),
+			mobile: $('.yaoqing_tel').val()
+		}
+		$('.yaoqing_maxk').css('display', 'none');
+		$.ajax({
+				url: 'http://v.jiaolianpai.com/user/User/addUserInfo',
+				type: 'post',
+				dataType: 'json',
+				data: yaoqing_namrel
+			})
+			.done(function(data) {
+				var data = data.data;
+				console.log('O(∩_∩)O哈哈~')
+				$('.yaoqing_name').val('');
+				$('.yaoqing_tel').val('');
+				$('.datadata').html(data)
+			})
+	});
+	// 发布邀请活动
 	$('#yaoqinghuodong').click(function() {
 		var dataval = {
+			data: $('.datadata').html(),
+			uid: 304,
 			title: $('.faqi_input').val(),
 			act_date: $('#pickdate').val(),
 			act_time: $('#picktime').val(),
 			remarks: $('.beizhu_input').val(),
-			venue_id: $('.qiuchang_input').val(),
+			venue_id: $('.qiuchang_inputid').html(),
 			join_member : $('.yaoqing').val()
 		}
 		$.ajax({
@@ -46,41 +107,7 @@ $(document).ready(function() {
 				data: dataval
 			})
 			.done(function(data) {
-				console.log('成功')
-					// uid  用户编号  
-					// join_member  邀请参赛人员，用逗号链接的用户编号 
-					// title标题 
-					// venue_id场馆编号
-					// act_date活动日期
-					// act_time活动时间
-					// remarks备注
+				console.log('发布邀请活动成功！O(∩_∩)O哈哈~')
 			})
-	})
-	var search = function() {
-			var title = $('#search_input').val();
-			$.ajax({
-				url: 'http://v.jiaolianpai.com/user/Venue/getList?title=' + title,
-				type: 'GET',
-				success: function(data) {
-					var data = jQuery.parseJSON(data);
-					var searchlist = data.data;
-					var html = "";
-					for (var i = 0; i < searchlist.length; i++) {
-						html += '<li>' + searchlist[i].title + '</li>';
-					};
-					$("#ball_list").html(html);
-				}
-			});
-		}
-		// 搜索
-	$('#search_btn').click(function() {
-		search();
-	})
-	$('#search_input').keyup(function() {
-			search();
-		})
-		// 清楚输入框
-	$('#x').click(function() {
-		$('#search_input').val('');
 	})
 });
