cc613946421573f5a6830538a5483d698c697e80
wangyonglei <549749526@qq.com>
2016-07-28 00:54:30 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/activitylist.js
+++ js/activitylist.js
@@ -1,4 +1,52 @@
 $(document).ready(function() {
+	//滚动加载
+	// $(window).scroll(function() {
+	// 		var docheight = $(document).height();
+	// 		var s_top = $(this).scrollTop();
+	// 		var nowheight = $(this).height();
+	// 		var page = 1;
+	// 		var limit = 1;
+	// 		if (docheight - s_top - nowheight < 5) {
+	// 			
+	// 		}
+	// 	})
+	var stop = true;
+	$(window).scroll(function() {
+		var totalheight = parseFloat($(window).height()) + parseFloat($(window).scrollTop());
+		if ($(document).height() <= totalheight) {
+			if (stop == true) {
+				stop = false;
+				var page = 2;
+				$.ajax({
+					url: 'http://v.jgsports.com.cn/user/Act/getList',
+					type: 'Get',
+					dataType: 'json',
+					data: {
+						page: page,
+						limit: 1,
+						type: 0,
+						uid: 304
+					},
+					success: function(data) {
+						var html;
+						var actlistdata = data.data;
+						for (var i = 0; i < actlistdata.length; i++) {
+							html += '<li><a href="activitydetail.html"><div class="al_img">';
+							if (!actlistdata[i].actPhotoAlbumList.picurl) {
+								html += '<img src="images/golfdetail1.jpg">';
+							}
+							html += '<div class="al_mask"></div><div class="al_mask_img">' + actlistdata[i].actPhotoAlbumNumber + '</div></div>' +
+								'<div class="alright_con"><h1>' + actlistdata[i].title + '</h1><p>' + actlistdata[i].act_date + '</p><p>' + actlistdata[i].venueTitle + '</p>' +
+								'<div class="al_km">' + actlistdata[i].z / 1000 + 'km</div>	</div></a></li>';
+						};
+						$("#Loading").before(html);
+						stop = true;
+						// page = page+1;
+					}
+				})
+			}
+		}
+	});
 	// 默认全部加载
 	$.ajax({
 			url: 'http://v.jgsports.com.cn/user/Act/getList',
@@ -7,15 +55,20 @@ $(document).ready(function() {
 			data: {
 				type: 0,
 				uid: 304,
+				page:1,
+				limit:5
 			},
 			success: function(data) {
-				var html = "";
+				var html = $('#activitylist').html();
 				var actlistdata = data.data;
-				for (var i = 0; i < actlistdata.length; i++) {
-					html += '<li><div class="al_img"><img src="' + actlistdata[i].actPhotoAlbumList.picurl + '">' +
-						'<div class="al_mask"></div><div class="al_mask_img">' + actlistdata[i].actPhotoAlbumList.actPhotoAlbumNumber + '</div></div>' +
+				for (var i = 0; i < 5; i++) {
+					html += '<li><a href="activitydetail.html"><div class="al_img">';
+					if (!actlistdata[i].actPhotoAlbumList.picurl) {
+						html += '<img src="images/golfdetail1.jpg">';
+					}
+					html += '<div class="al_mask"></div><div class="al_mask_img">' + actlistdata[i].actPhotoAlbumNumber + '</div></div>' +
 						'<div class="alright_con"><h1>' + actlistdata[i].title + '</h1><p>' + actlistdata[i].act_date + '</p><p>' + actlistdata[i].venueTitle + '</p>' +
-						'<div class="al_km">' + actlistdata[i].z / 1000 + 'km</div>	</div>	</li>';
+						'<div class="al_km">' + actlistdata[i].z / 1000 + 'km</div>	</div></a></li>';
 				};
 				$("#activitylist").html(html);
 			}
@@ -34,17 +87,7 @@ $(document).ready(function() {
 			type: 'Get',
 			dataType: 'json',
 			data: actdata,
-			success: function(data) {
-				var html = "";
-				var actlistdata = data.data;
-				for (var i = 0; i < actlistdata.length; i++) {
-					html += '<li><div class="al_img"><img src="' + actlistdata[i].actPhotoAlbumList.picurl + '">' +
-						'<div class="al_mask"></div><div class="al_mask_img">' + actlistdata[i].actPhotoAlbumList.actPhotoAlbumNumber + '</div></div>' +
-						'<div class="alright_con"><h1>' + actlistdata[i].title + '</h1><p>' + actlistdata[i].act_date + '</p><p>' + actlistdata[i].venueTitle + '</p>' +
-						'<div class="al_km">' + actlistdata[i].z / 1000 + 'km</div>	</div>	</li>';
-				};
-				$("#activitylist").html(html);
-			}
+			success: function(data) {}
 		})
 	})
 })
