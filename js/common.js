380691d325dfb550848e047ea99a180275a64c2c
wangyonglei <549749526@qq.com>
2016-08-06 00:21:33 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/common.js
+++ js/common.js
@@ -1,12 +1,22 @@
 window.onload = function() {
-	 var locationUrl =window.location.href;
+	(function($) {
+		$.getUrlParam = function(name) {
+			var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+			var r = window.location.search.substr(1).match(reg);
+			if (r != null) return unescape(r[2]);
+			return null;
+		}
+	})(jQuery);
+	var code = $.getUrlParam('code');
+	var locationUrl = window.location.href;
 	function is_weixin() {
 		var ua = navigator.userAgent.toLowerCase();
 		if (ua.match(/MicroMessenger/i) == "micromessenger") {
-			location= 'http://v.jgsports.com.cn/user/Act/getCode?backUri='+locationUrl;
+			if (!code) {
+				location = 'http://v.jgsports.com.cn/user/Act/getCode?backUri=' + locationUrl;
+			}
 		} else {
-			 
-			return false;
+			
 		}
 	}
 	is_weixin();
