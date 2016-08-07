6da9a9cee1b56383af6ab2107c2ad41f8ac35f01
wangyonglei <549749526@qq.com>
2016-08-07 23:49:25 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/card.js
+++ js/card.js
@@ -24,7 +24,7 @@ window.onload = function() {
 						},
 					})
 					.done(function(data) {
-						alert(code);
+						// alert(code);
 						// location = 'http://m.jgsports.com.cn/card.html'
 					})
 			}
