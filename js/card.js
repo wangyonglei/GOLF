6a15b55919f2b8cadcdf2a0a12469eb2b3caf268
wangyonglei <549749526@qq.com>
2016-08-08 14:35:47 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/card.js
+++ js/card.js
@@ -1,4 +1,42 @@
-window.onload = function() {
+// window.onload = function() {
+// 	(function($) {
+// 		$.getUrlParam = function(name) {
+// 			var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+// 			var r = window.location.search.substr(1).match(reg);
+// 			if (r != null) return unescape(r[2]);
+// 			return null;
+// 		}
+// 	})(jQuery);
+// 	var code = $.getUrlParam('code');
+// 	var locationUrl = window.location.href;
+// 	function is_weixin() {
+// 		var ua = navigator.userAgent.toLowerCase();
+// 		if (ua.match(/MicroMessenger/i) == "micromessenger") {
+// 			if (!code) {
+// 				location = 'http://v.jgsports.com.cn/user/Act/getCode?backUri=' + locationUrl;
+// 			} else {
+// 				$.ajax({
+// 						url: 'http://v.jgsports.com.cn//user/User/getUserInfo',
+// 						type: 'Get',
+// 						// dataType: 'json',
+// 						data: {
+// 							code: code
+// 						},
+// 					})
+// 					.done(function(data) {
+// 						// alert(code);
+// 						// location = 'http://m.jgsports.com.cn/card.html'
+// 					})
+// 			}
+// 		} else {
+// 			alert('请在微信客户端打开！')
+// 		}
+// 	}
+// 	is_weixin();
+// }
+
+
+jQuery(document).ready(function($) {
 	(function($) {
 		$.getUrlParam = function(name) {
 			var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
@@ -24,8 +62,7 @@ window.onload = function() {
 						},
 					})
 					.done(function(data) {
-						// alert(code);
-						// location = 'http://m.jgsports.com.cn/card.html'
+						
 					})
 			}
 		} else {
@@ -33,7 +70,10 @@ window.onload = function() {
 		}
 	}
 	is_weixin();
-}
+});
+	
+
+
 
 
 
--- js/common.js
+++ js/common.js
@@ -1,50 +1,39 @@
-// window.onload = function() {
-// 	(function($) {
-// 		$.getUrlParam = function(name) {
-// 			var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-// 			var r = window.location.search.substr(1).match(reg);
-// 			if (r != null) return unescape(r[2]);
-// 			return null;
-// 		}
-// 	})(jQuery);
-// 	var code = $.getUrlParam('code');
-// 	var locationUrl = window.location.href;
-// 	function is_weixin() {
-// 		var ua = navigator.userAgent.toLowerCase();
-// 		if (ua.match(/MicroMessenger/i) == "micromessenger") {
-// 			if (!code) {
-// 				location = 'http://v.jgsports.com.cn/user/Act/getCode?backUri=' + locationUrl;
-// 			} else {
-// 				$.ajax({
-// 						url: 'http://v.jgsports.com.cn//user/User/getUserInfo',
-// 						type: 'Get',
-// 						// dataType: 'json',
-// 						data: {
-// 							code: code
-// 						},
-// 					})
-// 					.done(function(data) {
-// 						alert(code);
-// 						// location = 'http://m.jgsports.com.cn/card.html'
-// 					})
-// 			}
-// 		} else {
-// 			alert('请在微信客户端打开！')
-// 		}
-// 	}
-// 	is_weixin();
-// }
+jQuery(document).ready(function($) {
+	(function($) {
+		$.getUrlParam = function(name) {
+			var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+			var r = window.location.search.substr(1).match(reg);
+			if (r != null) return unescape(r[2]);
+			return null;
+		}
+	})(jQuery);
+	var code = $.getUrlParam('code');
+	var locationUrl = window.location.href;
+	function is_weixin() {
+		var ua = navigator.userAgent.toLowerCase();
+		if (ua.match(/MicroMessenger/i) == "micromessenger") {
+			if (!code) {
+				location = 'http://v.jgsports.com.cn/user/Act/getCode?backUri=' + locationUrl;
+			} else {
+				$.ajax({
+						url: 'http://v.jgsports.com.cn//user/User/getUserInfo',
+						type: 'Get',
+						// dataType: 'json',
+						data: {
+							code: code
+						},
+					})
+					.done(function(data) {
+						alert(code);
+						// location = 'http://m.jgsports.com.cn/card.html'
+					})
+			}
+		} else {
+			alert('请在微信客户端打开！')
+		}
+	}
+	is_weixin();
+});
+	
 
 
-$.ajax({
-		url: 'http://v.jgsports.com.cn//user/User/getUserInfo',
-		type: 'Get',
-		// dataType: 'json',
-		data: {
-			// code: code
-		},
-	})
-	.done(function(data) {
-		// alert(code);
-		// location = 'http://m.jgsports.com.cn/card.html'
-	})
