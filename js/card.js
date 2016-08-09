71da5879c2df4ee49d81672397c7f0025bd58567
wangyonglei <549749526@qq.com>
2016-08-09 16:59:36 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/card.js
+++ js/card.js
@@ -1,4 +1,4 @@
-// window.onload = function() {
+// jQuery(document).ready(function($) {
 // 	(function($) {
 // 		$.getUrlParam = function(name) {
 // 			var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
@@ -8,35 +8,36 @@
 // 		}
 // 	})(jQuery);
 // 	var code = $.getUrlParam('code');
-// 	var locationUrl = window.location.href;
-// 	function is_weixin() {
-// 		var ua = navigator.userAgent.toLowerCase();
-// 		if (ua.match(/MicroMessenger/i) == "micromessenger") {
-// 			if (!code) {
-// 				location = 'http://v.jgsports.com.cn/user/Act/getCode?backUri=' + locationUrl;
-// 			} else {
-// 				$.ajax({
-// 						url: 'http://v.jgsports.com.cn//user/User/getUserInfo',
-// 						type: 'Get',
-// 						// dataType: 'json',
-// 						data: {
-// 							code: code
-// 						},
-// 					})
-// 					.done(function(data) {
-// 						// alert(code);
-// 						// location = 'http://m.jgsports.com.cn/card.html'
-// 					})
-// 			}
-// 		} else {
-// 			alert('请在微信客户端打开！')
-// 		}
-// 	}
-// 	is_weixin();
-// }
+
+// 	$.ajax({
+// 			url: 'http://v.jgsports.com.cn/user/User/getNewUserInfo',
+// 			// url: 'http://v.jgsports.com.cn//user/User/getUserInfo',
+// 			type: 'Get',
+// 			dataType: 'json',
+// 			// jsonp:callback,
+// 			data: {
+// 				code: code
+// 			},
+// 		})
+// 		.done(function(data) {
+// 			var carddata = data.data;
+// 			var html = '';
+// 			html +='<div class="card_ban"><div class="card_img"><img src="'+carddata.avatar+'"></div><div class="card_con"><div class="xian"><p></p><p>·</p><p></p></div><div class="card_name">'+carddata.rname+'</div><div class="card_des">'+carddata.signature+'</div><div class="card_qiu"><span>球龄</span>'+carddata.ball_age+'<span>差点</span>'+carddata.almost+'</div></div></div><div class="card_vip">'+carddata.membership+'</div><div class="card_tel">'+carddata.tel+'</div><div class="card_email">'+carddata.email+'</div><div class="hengxian"><p></p><p>·</p><p></p></div><div class="card_company">公司：'+carddata.company_describe+'</div><div class="card_post">职务：</div><div class="card_city">城市：'+carddata.city+'</div><div class="card_resources">资源：'+carddata.resources+'</div><div class="card_btn"><button class="card_but">编辑</button></div></div>'
+// 			$('.card').html(html)
+// 		})
 
 
-jQuery(document).ready(function($) {
+
+
+
+// });
+	
+
+
+
+
+$(document).ready(function($) {
+	console.log(1)
 	(function($) {
 		$.getUrlParam = function(name) {
 			var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
@@ -46,168 +47,64 @@ jQuery(document).ready(function($) {
 		}
 	})(jQuery);
 	var code = $.getUrlParam('code');
+	var ex_code = getCookie("ex_code");
+	var ex_mobile = getCookie("ex_mobile");
+	var ex_uid = getCookie("ex_uid");
+	if (!ex_code && !ex_mobile  && !ex_uid) {
+		console.log(2)
+		
+		var locationUrl = window.location.href;
+		var ua = navigator.userAgent.toLowerCase();
+		if (ua.match(/MicroMessenger/i) == "micromessenger") {
+			console.log(3)
+			if (!code) {
+				console.log(4)
+				location = 'http://v.jgsports.com.cn/user/Act/getCode?backUri=' + locationUrl;
+			} else {
+				console.log(5)
+				$.ajax({
+					url: 'http://v.jgsports.com.cn/user/User/login',
+					type: 'get',
+					dataType: 'json',
+					// jsonp: 'callback',
+					data: {
+						code: code
+					},
+					success: function(data) {
+						console.log(6)
+						// location = 'http://v.jgsports.com.cn/GOLF/card.html';
+					}
+				})
+			}
+		} else {
+			console.log(7)
+			alert('请在微信客户端打开！')
+		}
+	} else{
+		console.log(8)
+		// location = 'http://v.jgsports.com.cn/GOLF/card.html';
 
-	$.ajax({
-			url: 'http://v.jgsports.com.cn/user/User/getNewUserInfo',
-			// url: 'http://v.jgsports.com.cn//user/User/getUserInfo',
-			type: 'Get',
-			dataType: 'json',
-			// jsonp:callback,
-			data: {
-				code: code
-			},
-		})
-		.done(function(data) {
-			var carddata = data.data;
-			var html = '';
-			html +='<div class="card_ban"><div class="card_img"><img src="'+carddata.avatar+'"></div><div class="card_con"><div class="xian"><p></p><p>·</p><p></p></div><div class="card_name">'+carddata.rname+'</div><div class="card_des">'+carddata.signature+'</div><div class="card_qiu"><span>球龄</span>'+carddata.ball_age+'<span>差点</span>'+carddata.almost+'</div></div></div><div class="card_vip">'+carddata.membership+'</div><div class="card_tel">'+carddata.tel+'</div><div class="card_email">'+carddata.email+'</div><div class="hengxian"><p></p><p>·</p><p></p></div><div class="card_company">公司：'+carddata.company_describe+'</div><div class="card_post">职务：</div><div class="card_city">城市：'+carddata.city+'</div><div class="card_resources">资源：'+carddata.resources+'</div><div class="card_btn"><button class="card_but">编辑</button></div></div>'
-			$('.card').html(html)
-		})
+			$.ajax({
+					url: 'http://v.jgsports.com.cn/user/User/getNewUserInfo',
+					// url: 'http://v.jgsports.com.cn//user/User/getUserInfo',
+					type: 'Get',
+					dataType: 'json',
+					// jsonp:callback,
+					data: {
+						code: code
+					},
+				})
+				.done(function(data) {
+					var carddata = data.data;
+					var html = '';
+					html +='<div class="card_ban"><div class="card_img"><img src="'+carddata.avatar+'"></div><div class="card_con"><div class="xian"><p></p><p>·</p><p></p></div><div class="card_name">'+carddata.rname+'</div><div class="card_des">'+carddata.signature+'</div><div class="card_qiu"><span>球龄</span>'+carddata.ball_age+'<span>差点</span>'+carddata.almost+'</div></div></div><div class="card_vip">'+carddata.membership+'</div><div class="card_tel">'+carddata.tel+'</div><div class="card_email">'+carddata.email+'</div><div class="hengxian"><p></p><p>·</p><p></p></div><div class="card_company">公司：'+carddata.company_describe+'</div><div class="card_post">职务：</div><div class="card_city">城市：'+carddata.city+'</div><div class="card_resources">资源：'+carddata.resources+'</div><div class="card_btn"><button class="card_but">编辑</button></div></div>'
+					$('.card').html(html)
+				})
 
 
 
 
-	// var locationUrl = window.location.href;
-	// function is_weixin() {
-	// 	var ua = navigator.userAgent.toLowerCase();
-	// 	if (ua.match(/MicroMessenger/i) == "micromessenger") {
-	// 		if (!code) {
-	// 			location = 'http://v.jgsports.com.cn/user/Act/getCode?backUri=' + locationUrl;
-	// 		} else {
-	// 			$.ajax({
-	// 					url: 'http://v.jgsports.com.cn//user/User/getUserInfo',
-	// 					type: 'Get',
-	// 					// dataType: 'json',
-	// 					data: {
-	// 						code: code
-	// 					},
-	// 				})
-	// 				.done(function(data) {
-						
-	// 				})
-	// 		}
-	// 	} else {
-	// 		alert('请在微信客户端打开！')
-	// 	}
-	// }
-	// is_weixin();
+	}
 });
-	
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// window.onload = function() {
-// 	// var ex_code = "ex_code";
-// 	// var ex_mobile = "ex_mobile";
-// 	// var ex_uid = "ex_uid";
-// 	// var ex_codeName = document.cookie.indexOf(ex_code + "=");
-// 	// var ex_mobileName = document.cookie.indexOf(ex_mobile + "=");
-// 	// var ex_uidName = document.cookie.indexOf(ex_uid + "=");
-// 	// if (ex_codeName != -1 && ex_mobileName != -1 && ex_uidName != -1) {
-// 	// 	return true;
-// 	// } else {
-// 	// }
-// 	var ex_code = document.cookie.indexOf("ex_code=");
-// 	var ex_mobile = document.cookie.indexOf("ex_mobile=");
-// 	var ex_uid = document.cookie.indexOf("ex_uid=");
-// 	if (ex_code == -1 && ex_mobile == -1 && ex_uid == -1) {
-// 		(function($) {
-// 			$.getUrlParam = function(name) {
-// 				var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-// 				var r = window.location.search.substr(1).match(reg);
-// 				if (r != null) return unescape(r[2]);
-// 				return null;
-// 			}
-// 		})(jQuery);
-// 		var code = $.getUrlParam('code');
-// 		var locationUrl = window.location.href;
-// 		function is_weixin() {
-// 			var ua = navigator.userAgent.toLowerCase();
-// 			if (ua.match(/MicroMessenger/i) == "micromessenger") {
-// 				if (!code) {
-// 					location = 'http://v.jgsports.com.cn/user/Act/getCode?backUri=' + locationUrl;
-// 				} else {
-
-// 					$.ajax({
-// 							url: 'http://v.jgsports.com.cn//user/User/getUserInfo',
-// 							type: 'Get',
-// 							// dataType: 'json',
-// 							data: {
-// 								code: code
-// 							},
-// 						})
-// 						.done(function(data) {
-// 							alert(code)
-
-// 						})
-
-
-
-// 					// location = 'http://m.jgsports.com.cn/card.html';
-// 				}
-// 			} else {
-// 				alert('请在微信客户端打开！')
-// 			}
-// 		}
-// 		is_weixin();
-// 	}
-// }
-
-
-
-
-
-// // window.onload = function() {
-// // 	(function($) {
-// // 		$.getUrlParam = function(name) {
-// // 			var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-// // 			var r = window.location.search.substr(1).match(reg);
-// // 			if (r != null) return unescape(r[2]);
-// // 			return null;
-// // 		}
-// // 	})(jQuery);
-// // 	var code = $.getUrlParam('code');
-
-// // 	$.ajax({
-// // 			url: 'http://v.jgsports.com.cn//user/User/getUserInfo',
-// // 			type: 'Get',
-// // 			// dataType: 'json',
-// // 			data: {
-// // 				code: code
-// // 			},
-// // 		})
-// // 		.done(function(data) {
-// // 			alert(code)
-
-// // 		})
-// // }
--- js/common.js
+++ js/common.js
@@ -1,28 +1,4 @@
-// jQuery(document).ready(function($) {
-	
-// 		(function($) {
-// 			$.getUrlParam = function(name) {
-// 				var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-// 				var r = window.location.search.substr(1).match(reg);
-// 				if (r != null) return unescape(r[2]);
-// 				return null;
-// 			}
-// 		})(jQuery);
-// 		var code = $.getUrlParam('code');
-// 		// var code = '9960f77cb12a585372a530a2eb730878';
 
-// 		var locationUrl = window.location.href;
-// 		var ua = navigator.userAgent.toLowerCase();
-// 		if (ua.match(/MicroMessenger/i) == "micromessenger") {
-// 			if (!code) {
-// 				location = 'http://v.jgsports.com.cn/user/Act/getCode?backUri=' + locationUrl;
-// 			} else{
-// 				location = 'http://m.jgsports.com.cn/card.html?code='+code;
-// 			}
-// 		} else {
-// 			alert('请在微信客户端打开！')
-// 		}
-// });
 $(document).ready(function($) {
 	console.log(1)
 	var ex_code = getCookie("ex_code");
