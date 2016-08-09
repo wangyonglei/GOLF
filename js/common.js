ecd6fd173d4c8fe78ba0660b57c53bced7ab6b92
wangyonglei <549749526@qq.com>
2016-08-09 16:48:38 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/common.js
+++ js/common.js
@@ -23,11 +23,13 @@
 // 			alert('请在微信客户端打开！')
 // 		}
 // });
-jQuery(document).ready(function($) {
+$(document).ready(function($) {
+	console.log(1)
 	var ex_code = getCookie("ex_code");
 	var ex_mobile = getCookie("ex_mobile");
 	var ex_uid = getCookie("ex_uid");
 	if (!ex_code && !ex_mobile  && !ex_uid) {
+		console.log(2)
 		(function($) {
 			$.getUrlParam = function(name) {
 				var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
@@ -40,9 +42,12 @@ jQuery(document).ready(function($) {
 		var locationUrl = window.location.href;
 		var ua = navigator.userAgent.toLowerCase();
 		if (ua.match(/MicroMessenger/i) == "micromessenger") {
+			console.log(3)
 			if (!code) {
+				console.log(4)
 				location = 'http://v.jgsports.com.cn/user/Act/getCode?backUri=' + locationUrl;
 			} else {
+				console.log(5)
 				$.ajax({
 					url: 'http://v.jgsports.com.cn/user/User/login',
 					type: 'get',
@@ -52,14 +57,17 @@ jQuery(document).ready(function($) {
 						code: code
 					},
 					success: function(data) {
+						console.log(6)
 						location = 'http://v.jgsports.com.cn/GOLF/card.html';
 					}
 				})
 			}
 		} else {
+			console.log(7)
 			alert('请在微信客户端打开！')
 		}
 	} else{
+		console.log(8)
 		location = 'http://v.jgsports.com.cn/GOLF/card.html';
 	}
 });
