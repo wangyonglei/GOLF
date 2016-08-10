14689e36a84c07836953ccfe479d816d97eac762
wangyonglei <549749526@qq.com>
2016-08-10 16:29:03 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/cart_edit.js
+++ js/cart_edit.js
@@ -113,7 +113,7 @@ $(document).ready(function($) {
 
 $(document).ready(function() {
 	// 球场列表
-	$('.huiji_s').click(function() {
+	$('body').on('click','huiji_s',function() {
 		$('.qiuchang_maxk').css('display', 'block');
 		$('.qiuchang_mask').css('display', 'block');
 
