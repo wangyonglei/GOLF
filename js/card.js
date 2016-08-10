7a79e8a502b29fd00c7ec20b4e457e4ed4f38079
wangyonglei <549749526@qq.com>
2016-08-10 10:36:13 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/card.js
+++ js/card.js
@@ -2,9 +2,28 @@ $(document).ready(function($) {
 	var ex_code = getCookie("ex_code");
 	var ex_mobile = getCookie("ex_mobile");
 	var ex_uid = getCookie("ex_uid");
-	// var loadCard = function() {
-	
-	// }
+	var loadCard = function() {
+		$.ajax({
+				url: 'http://v.jgsports.com.cn/user/User/getUserInfo',
+				type: 'Get',
+				dataType: 'json',
+				data: {
+					code: code
+				}
+			})
+			.done(function(data) {
+				var carddata = data.data;
+				var html = '';
+				html += '<div class="card_ban"><div class="card_img"><img src="' + carddata.avatar + '"></div><div class="card_con"><div class="xian"><p></p><p>·</p><p></p></div><div class="card_name">' + carddata.rname + '</div><div class="card_des">' + carddata.signature + '</div><div class="card_qiu"><span>球龄</span>' + carddata.ball_age + '<span>差点</span>' + carddata.almost + '</div></div></div><div class="card_vip">' + carddata.membership + '</div><div class="card_tel">' + carddata.tel + '</div><div class="card_email">' + carddata.email + '</div><div class="hengxian"><p></p><p>·</p><p></p></div><div class="card_company">公司：' + carddata.company_describe + '</div><div class="card_post">职务：</div><div class="card_city">城市：' + carddata.city + '</div><div class="card_resources">资源：' + carddata.resources + '</div><div class="card_btn"><a href="cart_edit.html?uid=' + carddata.uid + '">编辑</a></div></div>'
+				$('.card').html(html)
+			})
+			.fail(function() {
+				console.log("error");
+			})
+			.always(function() {
+				console.log("complete");
+			});
+	}
 	if (!ex_code && !ex_mobile && !ex_uid) {
 		var code = decodeURIComponent((new RegExp('[?|&]code=' + '([^&;]+?)(&|#|;|$)', "ig").exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
 		var locationUrl = window.location.href;
@@ -22,26 +41,7 @@ $(document).ready(function($) {
 						}
 					})
 					.done(function(data) {
-						$.ajax({
-									url: 'http://v.jgsports.com.cn/user/User/getUserInfo',
-									type: 'Get',
-									dataType: 'json',
-									data: {
-										code: code
-									}
-								})
-								.done(function(data) {
-									var carddata = data.data;
-									var html = '';
-									html += '<div class="card_ban"><div class="card_img"><img src="' + carddata.avatar + '"></div><div class="card_con"><div class="xian"><p></p><p>·</p><p></p></div><div class="card_name">' + carddata.rname + '</div><div class="card_des">' + carddata.signature + '</div><div class="card_qiu"><span>球龄</span>' + carddata.ball_age + '<span>差点</span>' + carddata.almost + '</div></div></div><div class="card_vip">' + carddata.membership + '</div><div class="card_tel">' + carddata.tel + '</div><div class="card_email">' + carddata.email + '</div><div class="hengxian"><p></p><p>·</p><p></p></div><div class="card_company">公司：' + carddata.company_describe + '</div><div class="card_post">职务：</div><div class="card_city">城市：' + carddata.city + '</div><div class="card_resources">资源：' + carddata.resources + '</div><div class="card_btn"><a href="cart_edit.html?uid=' + carddata.uid + '">编辑</a></div></div>'
-									$('.card').html(html)
-								})
-								.fail(function() {
-									console.log("error");
-								})
-								.always(function() {
-									console.log("complete");
-								});
+						loadCard()
 					})
 					.fail(function() {
 						console.log("error");
@@ -54,6 +54,6 @@ $(document).ready(function($) {
 			alert('请在微信客户端打开！')
 		}
 	} else {
-		// loadCard()
+		loadCard()
 	}
 });
