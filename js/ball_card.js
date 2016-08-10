880d842f3131b5e4e2de9e4081f81f5826a0c794
wangyonglei <549749526@qq.com>
2016-08-10 23:31:34 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

---
+++ js/ball_card.js
@@ -0,0 +1,72 @@
+$(document).ready(function($) {
+
+	var ex_code = getCookie("ex_code");
+	var ex_mobile = getCookie("ex_mobile");
+	var ex_uid = getCookie("ex_uid");
+	var ballCar = function() {
+		$('.team_addbut').click(function(){
+			var ballteam = {
+				title: $('.ballteam_input').val()
+			}
+			$.ajax({
+					url: 'http://v.jgsports.com.cn/user/Team/getList',
+					type: 'get',
+					dataType: 'json',
+					data: ballteam
+				})
+				.done(function(data) {
+					var html = '';
+html += '<div class="wai"><div class="duan ball_card_title">大鹏队（2人）<p class="xiajiao"></p></div>'+
+'<ul class="ball_card_people"><li><div class="people_img"><img src="images/card_img.jpg"></div><p>大鹏</p></li></ul></div>'+
+'<div class="wai"><div class="duan ball_card_title">大鹏队（2人）<p class="xiajiao"></p></div>'+
+'<ul class="ball_card_people"><li><div class="people_img"><img src="images/card_img.jpg"></div><p>大鹏</p></li>'+
+'<li><div class="people_img"><img src="images/jia.png"></div></li>'+
+'<li><div class="people_img"><img src="images/jian.png"></div></li></ul></div>'
+					$('#ball_card').html(html)
+				})
+		})
+	}
+	if (!ex_code && !ex_mobile && !ex_uid) {
+		var code = decodeURIComponent((new RegExp('[?|&]code=' + '([^&;]+?)(&|#|;|$)', "ig").exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
+		var locationUrl = window.location.href;
+		var ua = navigator.userAgent.toLowerCase();
+		if (ua.match(/MicroMessenger/i) == "micromessenger") {
+			if (!code) {
+				location = 'http://v.jgsports.com.cn/user/Act/getCode?backUri=' + locationUrl;
+			} else {
+				$.ajax({
+						url: 'http://v.jgsports.com.cn/user/User/login',
+						type: 'get',
+						dataType: 'json',
+						data: {
+							code: code
+						}
+					})
+					.done(function(data) {
+						ballCar()
+					})
+					.fail(function() {
+						console.log("error");
+					})
+					.always(function() {
+						console.log("complete");
+					});
+			}
+		} else {
+			alert('请在微信客户端打开！')
+		}
+	} else {
+		ballCar()
+	}
+
+
+
+
+
+
+
+
+
+
+	
+})
