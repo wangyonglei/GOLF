cafab6fa4be55b4512bfad9e8aee96c6c0f2a746
wangyonglei <549749526@qq.com>
2016-08-11 00:02:24 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/ball_card.js
+++ js/ball_card.js
@@ -1,43 +1,36 @@
 $(document).ready(function($) {
-
 	var ex_code = getCookie("ex_code");
 	var ex_mobile = getCookie("ex_mobile");
 	var ex_uid = getCookie("ex_uid");
 	var ballCar = function() {
-		$('.search_btn').click(function(){
-			var ballteam = {
-				title: $('.ballteam_input').val()
-			}
-			$.ajax({
-					url: 'http://v.jgsports.com.cn/user/Team/getList',
-					type: 'get',
-					dataType: 'json',
-					data: ballteam
-				})
-				.done(function(data) {
-					var balldata =data.data;
-					var html = '';
-// html += '<div class="wai"><div class="duan ball_card_title">大鹏队（2人）<p class="xiajiao"></p></div>'+
-// '<ul class="ball_card_people"><li><div class="people_img"><img src="images/card_img.jpg"></div><p>大鹏</p></li></ul></div>'
-
-html +='<div class="wai"><div class="duan ball_card_title">大鹏队（'+balldata.membersNumber+'人）<p class="xiajiao"></p></div><ul class="ball_card_people">'
-// if (	balldata.membersList.length == 0 ) {
-// 	html +='<li>暂无</li>'
-// }else{
-	for (var i = 0; i <balldata.membersList.length; i++) {
-		html +='<li><div class="people_img"><img src="'+balldata.membersList[i].avatar+'"></div><p>'+balldata.membersList[i].rname+'</p></li>'
-	}
-// }
-if (balldata.membersList.teamMaster == 1) {
-html +='<li><div class="people_img"><img src="images/jia.png"></div></li><li><div class="people_img"><img src="images/jian.png"></div></li>'
-
-}else{
-
-}
-html +='</ul></div>'
-					$('#ball_card').html(html)
-				})
-		})
+		var ballteam = {
+			title: $('.ballteam_input').val()
+		}
+		$.ajax({
+				url: 'http://v.jgsports.com.cn/user/Team/getList',
+				type: 'get',
+				dataType: 'json',
+				data: ballteam
+			})
+			.done(function(data) {
+				var balldata = data.data;
+				var html = '';
+				// html += '<div class="wai"><div class="duan ball_card_title">大鹏队（2人）<p class="xiajiao"></p></div>'+
+				// '<ul class="ball_card_people"><li><div class="people_img"><img src="images/card_img.jpg"></div><p>大鹏</p></li></ul></div>'
+				html += '<div class="wai"><div class="duan ball_card_title">大鹏队（' + balldata.membersNumber + '人）<p class="xiajiao"></p></div><ul class="ball_card_people">'
+					// if (	balldata.membersList.length == 0 ) {
+					// 	html +='<li>暂无</li>'
+					// }else{
+				for (var i = 0; i < balldata.membersList.length; i++) {
+					html += '<li><div class="people_img"><img src="' + balldata.membersList[i].avatar + '"></div><p>' + balldata.membersList[i].rname + '</p></li>'
+				}
+				// }
+				if (balldata.membersList.teamMaster == 1) {
+					html += '<li><div class="people_img"><img src="images/jia.png"></div></li><li><div class="people_img"><img src="images/jian.png"></div></li>'
+				} else {}
+				html += '</ul></div>'
+				$('#ball_card').html(html)
+			})
 	}
 	if (!ex_code && !ex_mobile && !ex_uid) {
 		var code = decodeURIComponent((new RegExp('[?|&]code=' + '([^&;]+?)(&|#|;|$)', "ig").exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
@@ -56,7 +49,10 @@ html +='</ul></div>'
 						}
 					})
 					.done(function(data) {
-						ballCar()
+						ballCar();
+						$('.search_btn').click(function() {
+							ballCar();
+						})
 					})
 					.fail(function() {
 						console.log("error");
@@ -69,17 +65,9 @@ html +='</ul></div>'
 			alert('请在微信客户端打开！')
 		}
 	} else {
-		ballCar()
+		ballCar();
+		$('.search_btn').click(function() {
+			ballCar();
+		})
 	}
-
-
-
-
-
-
-
-
-
-
-	
 })
