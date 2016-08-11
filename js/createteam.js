a7b86aada2fd2035a3b2053d5f680dcb3b9c77df
wangyonglei <549749526@qq.com>
2016-08-11 21:46:12 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/createteam.js
+++ js/createteam.js
@@ -51,7 +51,7 @@ $(document).ready(function($) {
 								})
 								.done(function(data) {
 									alert(1)
-									window.location.href = 'http://v.jgsports.com.cn/ball_card.html'
+									window.location.href = 'http://v.jgsports.com.cn/Golf/ball_card.html'
 								})
 						})
 
@@ -83,7 +83,7 @@ $(document).ready(function($) {
 				})
 				.done(function(data) {
 					alert(3)
-					window.location.href = 'http://v.jgsports.com.cn/ball_card.html'
+					window.location.href = 'http://v.jgsports.com.cn/GOLF/ball_card.html'
 				})
 		})
 
