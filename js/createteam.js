883c79fe7ca634ec91db676b977e1abdbaa5b1b2
wangyonglei <549749526@qq.com>
2016-08-13 12:57:16 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/createteam.js
+++ js/createteam.js
@@ -2,23 +2,23 @@ $(document).ready(function($) {
 	var ex_code = getCookie("ex_code");
 	var ex_mobile = getCookie("ex_mobile");
 	var ex_uid = getCookie("ex_uid");
-	// var createTeam = function() {
-	// 	$('.team_addbut').click(function() {
-	// 		var createteam = {
-	// 			title: $('.team_input').val(),
-	// 			profile: $('.team_con').val()
-	// 		}
-	// 		$.ajax({
-	// 				url: 'http://v.jgsports.com.cn/user/Team/addTeam',
-	// 				type: 'post',
-	// 				dataType: 'json',
-	// 				data: createteam
-	// 			})
-	// 			.done(function(data) {
-	// 				window.location.href = 'http://v.jgsports.com.cn/ball_card.html'
-	// 			})
-	// 	})
-	// }
+	var createTeam = function() {
+		$('.team_addbut').click(function() {
+			var createteam = {
+				title: $('.team_input').val(),
+				profile: $('.team_con').val()
+			}
+			$.ajax({
+					url: 'http://v.jgsports.com.cn/user/Team/addTeam',
+					type: 'post',
+					dataType: 'json',
+					data: createteam
+				})
+				.done(function(data) {
+					// window.location.href = 'http://v.jgsports.com.cn/GOLF/ball_card.html'
+				})
+		})
+	}
 	if (!ex_code && !ex_mobile && !ex_uid) {
 		var code = decodeURIComponent((new RegExp('[?|&]code=' + '([^&;]+?)(&|#|;|$)', "ig").exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
 		var locationUrl = window.location.href;
@@ -37,21 +37,7 @@ $(document).ready(function($) {
 					})
 					.done(function(data) {
 						
-						$('.team_addbut').click(function() {
-							var createteam = {
-								title: $('.team_input').val(),
-								profile: $('.team_con').val()
-							}
-							$.ajax({
-									url: 'http://v.jgsports.com.cn/user/Team/addTeam',
-									type: 'post',
-									dataType: 'json',
-									data: createteam
-								})
-								.done(function(data) {
-									window.location.href = 'http://v.jgsports.com.cn/GOLF/ball_card.html'
-								})
-						})
+						createTeam();
 
 
 					})
@@ -67,21 +53,7 @@ $(document).ready(function($) {
 		}
 	} else {
 		
-		$('.team_addbut').click(function() {
-			var createteam = {
-				title: $('.team_input').val(),
-				profile: $('.team_con').val()
-			}
-			$.ajax({
-					url: 'http://v.jgsports.com.cn/user/Team/addTeam',
-					type: 'post',
-					dataType: 'json',
-					data: createteam
-				})
-				.done(function(data) {
-					window.location.href = 'http://v.jgsports.com.cn/GOLF/ball_card.html'
-				})
-		})
+		createTeam();
 
 
 	}
