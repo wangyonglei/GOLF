d20bf4cef547421017efebed6327840335dc3409
wangyonglei <549749526@qq.com>
2016-08-14 20:46:25 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/activitylist.js
+++ js/activitylist.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	// 已经报名
-	// 默认全部加载10条
+	// 默认全部加载6条
 	$.ajax({
 			url: 'http://v.jgsports.com.cn/user/Act/getList',
 			type: 'Get',
@@ -26,10 +26,11 @@ $(document).ready(function() {
 				};
 				// $("#activitylist").html(html);
 				$('.Loading0').before(html)
+				scrollpage(0);
 			}
 		})
 		
-	var scrollpage = function(){
+	var scrollpage = function(type){
 			// 滑屏加载数据
 		var stop = true;
 		page = 2;
@@ -45,9 +46,8 @@ $(document).ready(function() {
 						data: {
 							page: page,
 							limit: 2,
-							type: 0,
+							type: type,
 							category:1
-							// uid: 304
 						},
 						success: function(data) {
 							var html = '';
@@ -61,13 +61,13 @@ $(document).ready(function() {
 									'<div class="alright_con"><h1>' + actlistdata[i].title + '</h1><p>' + actlistdata[i].act_date + '</p><p>' + actlistdata[i].venueTitle + '</p>' +
 									'<div class="al_km">' + actlistdata[i].z / 1000 + 'km</div>	</div></a></li>';
 							};
-							$(".Loading").before(html);
+							$(".Loading"+type).before(html);
 							stop = true;
 							page++;
 							if (data.msg=="暂无活动信息") {
-								$(".Loading0").html("全部加载完！");
+								$(".Loading"+type).html("全部加载完！");
 							}else{
-								$(".Loading0").html("Loading...");
+								$(".Loading"+type).html("Loading...");
 
 							}
 						}
@@ -76,46 +76,46 @@ $(document).ready(function() {
 			}
 		});
 	}
-	scrollpage();
 
 
 
-		// 点击加载各类 0为全部 ， 1 为本周  ，2为本月  3为球场
-	$(".al_con").eq(0).show();
-	$(".al_title li").one('click',function() {
-			$(this).addClass("cur").siblings().removeClass('cur');
-			index = $(this).index();
-			$(".al_con").hide().eq($(this).index()).show();
-			var actdata = {
-				type: index,
-				page: 1,
-				limit: 2,
-				category:1
-				// uid: 304,
 
-			}
-			$.ajax({
-				url: 'http://v.jgsports.com.cn/user/Act/getList',
-				type: 'Get',
-				dataType: 'json',
-				data: actdata,
-				success: function(data) {
-					// $("#activitylist").html('')
-					var html = '';
-					var actlistdata = data.data;
-					for (var i = 0; i < 2; i++) {
-						html += '<li><a href="activitydetail.html"><div class="al_img">';
-						if (!actlistdata[i].actPhotoAlbumList.picurl) {
-							html += '<img src="images/golfdetail1.jpg">';
-						}
-						html += '<div class="al_mask"></div><div class="al_mask_img">' + actlistdata[i].actPhotoAlbumNumber + '</div></div>' +
-							'<div class="alright_con"><h1>' + actlistdata[i].title + '</h1><p>' + actlistdata[i].act_date + '</p><p>' + actlistdata[i].venueTitle + '</p>' +
-							'<div class="al_km">' + actlistdata[i].z / 1000 + 'km</div>	</div></a></li>';
-					};
-					$(".Loading"+index).before(html);
-				}
-			});
-		})
+	// 	// 点击加载各类 0为全部 ， 1 为本周  ，2为本月  3为球场
+	// $(".al_con").eq(0).show();
+	// $(".al_title li").click(function() {
+	// 		$(this).addClass("cur").siblings().removeClass('cur');
+	// 		index = $(this).index();
+	// 		$(".al_con").hide().eq($(this).index()).show();
+	// 		var actdata = {
+	// 			type: index,
+	// 			page: 1,
+	// 			limit: 2,
+	// 			category:1
+	// 			// uid: 304,
+
+	// 		}
+	// 		$.ajax({
+	// 			url: 'http://v.jgsports.com.cn/user/Act/getList',
+	// 			type: 'Get',
+	// 			dataType: 'json',
+	// 			data: actdata,
+	// 			success: function(data) {
+	// 				// $("#activitylist").html('')
+	// 				var html = '';
+	// 				var actlistdata = data.data;
+	// 				for (var i = 0; i < 2; i++) {
+	// 					html += '<li><a href="activitydetail.html"><div class="al_img">';
+	// 					if (!actlistdata[i].actPhotoAlbumList.picurl) {
+	// 						html += '<img src="images/golfdetail1.jpg">';
+	// 					}
+	// 					html += '<div class="al_mask"></div><div class="al_mask_img">' + actlistdata[i].actPhotoAlbumNumber + '</div></div>' +
+	// 						'<div class="alright_con"><h1>' + actlistdata[i].title + '</h1><p>' + actlistdata[i].act_date + '</p><p>' + actlistdata[i].venueTitle + '</p>' +
+	// 						'<div class="al_km">' + actlistdata[i].z / 1000 + 'km</div>	</div></a></li>';
+	// 				};
+	// 				$(".Loading"+index).before(html);
+	// 			}
+	// 		});
+	// 	})
 
 
 
