7790f8e7a396c5a53234db1e8231d29ac0ac7f28
wangyonglei <549749526@qq.com>
2016-08-14 20:52:38 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/activitylist.js
+++ js/activitylist.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	 $(".al_con0").show();
 	// 已经报名
 	// 默认全部加载6条
 	$.ajax({
@@ -8,14 +9,14 @@ $(document).ready(function() {
 			data: {
 				type: 0,
 				page: 1,
-				limit: 2,
+				limit: 6,
 				category:1
 			},
 			success: function(data) {
 				// var html = $('#activitylist').html();
 				var html = '';
 				var actlistdata = data.data;
-				for (var i = 0; i < 2; i++) {
+				for (var i = 0; i < 6; i++) {
 					html += '<li><a href="activitydetail.html"><div class="al_img">';
 					if (!actlistdata[i].actPhotoAlbumList.picurl) {
 						html += '<img src="images/golfdetail1.jpg">';
@@ -26,62 +27,62 @@ $(document).ready(function() {
 				};
 				// $("#activitylist").html(html);
 				$('.Loading0').before(html)
+				scrollpage(0);
 			}
 		})
 		
-	// var scrollpage = function(type){
-	// 		// 滑屏加载数据
-	// 	var stop = true;
-	// 	page = 2;
-	// 	$(window).scroll(function() {
-	// 		var totalheight = parseFloat($(window).height()) + parseFloat($(window).scrollTop());
-	// 		if ($(document).height() <= totalheight) {
-	// 			if (stop == true) {
-	// 				stop = false;
-	// 				$.ajax({
-	// 					url: 'http://v.jgsports.com.cn/user/Act/getList',
-	// 					type: 'Get',
-	// 					dataType: 'json',
-	// 					data: {
-	// 						page: page,
-	// 						limit: 2,
-	// 						type: type,
-	// 						category:1
-	// 					},
-	// 					success: function(data) {
-	// 						var html = '';
-	// 						var actlistdata = data.data;
-	// 						for (var i = 0; i < actlistdata.length; i++) {
-	// 							html += '<li><a href="activitydetail.html"><div class="al_img">';
-	// 							if (!actlistdata[i].actPhotoAlbumList.picurl) {
-	// 								html += '<img src="images/golfdetail1.jpg">';
-	// 							}
-	// 							html += '<div class="al_mask"></div><div class="al_mask_img">' + actlistdata[i].actPhotoAlbumNumber + '</div></div>' +
-	// 								'<div class="alright_con"><h1>' + actlistdata[i].title + '</h1><p>' + actlistdata[i].act_date + '</p><p>' + actlistdata[i].venueTitle + '</p>' +
-	// 								'<div class="al_km">' + actlistdata[i].z / 1000 + 'km</div>	</div></a></li>';
-	// 						};
-	// 						$(".Loading"+type).before(html);
-	// 						stop = true;
-	// 						page++;
-	// 						if (data.msg=="暂无活动信息") {
-	// 							$(".Loading"+type).html("全部加载完！");
-	// 						}else{
-	// 							$(".Loading"+type).html("Loading...");
+	var scrollpage = function(type){
+			// 滑屏加载数据
+		var stop = true;
+		page = 2;
+		$(window).scroll(function() {
+			var totalheight = parseFloat($(window).height()) + parseFloat($(window).scrollTop());
+			if ($(document).height() <= totalheight) {
+				if (stop == true) {
+					stop = false;
+					$.ajax({
+						url: 'http://v.jgsports.com.cn/user/Act/getList',
+						type: 'Get',
+						dataType: 'json',
+						data: {
+							page: page,
+							limit: 2,
+							type: type,
+							category:1
+						},
+						success: function(data) {
+							var html = '';
+							var actlistdata = data.data;
+							for (var i = 0; i < actlistdata.length; i++) {
+								html += '<li><a href="activitydetail.html"><div class="al_img">';
+								if (!actlistdata[i].actPhotoAlbumList.picurl) {
+									html += '<img src="images/golfdetail1.jpg">';
+								}
+								html += '<div class="al_mask"></div><div class="al_mask_img">' + actlistdata[i].actPhotoAlbumNumber + '</div></div>' +
+									'<div class="alright_con"><h1>' + actlistdata[i].title + '</h1><p>' + actlistdata[i].act_date + '</p><p>' + actlistdata[i].venueTitle + '</p>' +
+									'<div class="al_km">' + actlistdata[i].z / 1000 + 'km</div>	</div></a></li>';
+							};
+							$(".Loading"+type).before(html);
+							stop = true;
+							page++;
+							if (data.msg=="暂无活动信息") {
+								$(".Loading"+type).html("全部加载完！");
+							}else{
+								$(".Loading"+type).html("Loading...");
 
-	// 						}
-	// 					}
-	// 				})
-	// 			}
-	// 		}
-	// 	});
-	// }
-	// 			scrollpage(0);
+							}
+						}
+					})
+				}
+			}
+		});
+	}
 
 
 
 
 	// 	// 点击加载各类 0为全部 ， 1 为本周  ，2为本月  3为球场
-	// $(".al_con").eq(0).show();
+	//
 	// $(".al_title li").click(function() {
 	// 		$(this).addClass("cur").siblings().removeClass('cur');
 	// 		index = $(this).index();
