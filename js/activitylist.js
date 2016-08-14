e5045eab5f5ed0ab60fcedc30bd107d79e1c9f83
wangyonglei <549749526@qq.com>
2016-08-14 20:49:41 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/activitylist.js
+++ js/activitylist.js
@@ -25,57 +25,57 @@ $(document).ready(function() {
 						'<div class="al_km">' + actlistdata[i].z / 1000 + 'km</div>	</div></a></li>';
 				};
 				// $("#activitylist").html(html);
-				$('.Loading').before(html)
+				$('.Loading0').before(html)
 			}
 		})
 		
-	var scrollpage = function(type){
-			// 滑屏加载数据
-		var stop = true;
-		page = 2;
-		$(window).scroll(function() {
-			var totalheight = parseFloat($(window).height()) + parseFloat($(window).scrollTop());
-			if ($(document).height() <= totalheight) {
-				if (stop == true) {
-					stop = false;
-					$.ajax({
-						url: 'http://v.jgsports.com.cn/user/Act/getList',
-						type: 'Get',
-						dataType: 'json',
-						data: {
-							page: page,
-							limit: 2,
-							type: type,
-							category:1
-						},
-						success: function(data) {
-							var html = '';
-							var actlistdata = data.data;
-							for (var i = 0; i < actlistdata.length; i++) {
-								html += '<li><a href="activitydetail.html"><div class="al_img">';
-								if (!actlistdata[i].actPhotoAlbumList.picurl) {
-									html += '<img src="images/golfdetail1.jpg">';
-								}
-								html += '<div class="al_mask"></div><div class="al_mask_img">' + actlistdata[i].actPhotoAlbumNumber + '</div></div>' +
-									'<div class="alright_con"><h1>' + actlistdata[i].title + '</h1><p>' + actlistdata[i].act_date + '</p><p>' + actlistdata[i].venueTitle + '</p>' +
-									'<div class="al_km">' + actlistdata[i].z / 1000 + 'km</div>	</div></a></li>';
-							};
-							$(".Loading"+type).before(html);
-							stop = true;
-							page++;
-							if (data.msg=="暂无活动信息") {
-								$(".Loading"+type).html("全部加载完！");
-							}else{
-								$(".Loading"+type).html("Loading...");
+	// var scrollpage = function(type){
+	// 		// 滑屏加载数据
+	// 	var stop = true;
+	// 	page = 2;
+	// 	$(window).scroll(function() {
+	// 		var totalheight = parseFloat($(window).height()) + parseFloat($(window).scrollTop());
+	// 		if ($(document).height() <= totalheight) {
+	// 			if (stop == true) {
+	// 				stop = false;
+	// 				$.ajax({
+	// 					url: 'http://v.jgsports.com.cn/user/Act/getList',
+	// 					type: 'Get',
+	// 					dataType: 'json',
+	// 					data: {
+	// 						page: page,
+	// 						limit: 2,
+	// 						type: type,
+	// 						category:1
+	// 					},
+	// 					success: function(data) {
+	// 						var html = '';
+	// 						var actlistdata = data.data;
+	// 						for (var i = 0; i < actlistdata.length; i++) {
+	// 							html += '<li><a href="activitydetail.html"><div class="al_img">';
+	// 							if (!actlistdata[i].actPhotoAlbumList.picurl) {
+	// 								html += '<img src="images/golfdetail1.jpg">';
+	// 							}
+	// 							html += '<div class="al_mask"></div><div class="al_mask_img">' + actlistdata[i].actPhotoAlbumNumber + '</div></div>' +
+	// 								'<div class="alright_con"><h1>' + actlistdata[i].title + '</h1><p>' + actlistdata[i].act_date + '</p><p>' + actlistdata[i].venueTitle + '</p>' +
+	// 								'<div class="al_km">' + actlistdata[i].z / 1000 + 'km</div>	</div></a></li>';
+	// 						};
+	// 						$(".Loading"+type).before(html);
+	// 						stop = true;
+	// 						page++;
+	// 						if (data.msg=="暂无活动信息") {
+	// 							$(".Loading"+type).html("全部加载完！");
+	// 						}else{
+	// 							$(".Loading"+type).html("Loading...");
 
-							}
-						}
-					})
-				}
-			}
-		});
-	}
-				scrollpage(0);
+	// 						}
+	// 					}
+	// 				})
+	// 			}
+	// 		}
+	// 	});
+	// }
+	// 			scrollpage(0);
 
 
 
