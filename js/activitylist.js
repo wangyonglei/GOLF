ad097065e5a381b4eaaaac91485b7fe14d64e633
wangyonglei <549749526@qq.com>
2016-08-14 20:55:23 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/activitylist.js
+++ js/activitylist.js
@@ -81,42 +81,43 @@ $(document).ready(function() {
 
 
 
-	// 	// 点击加载各类 0为全部 ， 1 为本周  ，2为本月  3为球场
-	//
-	// $(".al_title li").click(function() {
-	// 		$(this).addClass("cur").siblings().removeClass('cur');
-	// 		index = $(this).index();
-	// 		$(".al_con").hide().eq($(this).index()).show();
-	// 		var actdata = {
-	// 			type: index,
-	// 			page: 1,
-	// 			limit: 2,
-	// 			category:1
-	// 			// uid: 304,
+		// 点击加载各类 0为全部 ， 1 为本周  ，2为本月  3为球场
+	
+	$(".al_title li").click(function() {
+			$(this).addClass("cur").siblings().removeClass('cur');
+			index = $(this).index();
+			$(".al_con").hide().eq($(this).index()).show();
+			var actdata = {
+				type: index,
+				page: 1,
+				limit: 2,
+				category:1
+				// uid: 304,
 
-	// 		}
-	// 		$.ajax({
-	// 			url: 'http://v.jgsports.com.cn/user/Act/getList',
-	// 			type: 'Get',
-	// 			dataType: 'json',
-	// 			data: actdata,
-	// 			success: function(data) {
-	// 				// $("#activitylist").html('')
-	// 				var html = '';
-	// 				var actlistdata = data.data;
-	// 				for (var i = 0; i < 2; i++) {
-	// 					html += '<li><a href="activitydetail.html"><div class="al_img">';
-	// 					if (!actlistdata[i].actPhotoAlbumList.picurl) {
-	// 						html += '<img src="images/golfdetail1.jpg">';
-	// 					}
-	// 					html += '<div class="al_mask"></div><div class="al_mask_img">' + actlistdata[i].actPhotoAlbumNumber + '</div></div>' +
-	// 						'<div class="alright_con"><h1>' + actlistdata[i].title + '</h1><p>' + actlistdata[i].act_date + '</p><p>' + actlistdata[i].venueTitle + '</p>' +
-	// 						'<div class="al_km">' + actlistdata[i].z / 1000 + 'km</div>	</div></a></li>';
-	// 				};
-	// 				$(".Loading"+index).before(html);
-	// 			}
-	// 		});
-	// 	})
+			}
+			$.ajax({
+				url: 'http://v.jgsports.com.cn/user/Act/getList',
+				type: 'Get',
+				dataType: 'json',
+				data: actdata,
+				success: function(data) {
+					// $("#activitylist").html('')
+					var html = '';
+					var actlistdata = data.data;
+					for (var i = 0; i < 2; i++) {
+						html += '<li><a href="activitydetail.html"><div class="al_img">';
+						if (!actlistdata[i].actPhotoAlbumList.picurl) {
+							html += '<img src="images/golfdetail1.jpg">';
+						}
+						html += '<div class="al_mask"></div><div class="al_mask_img">' + actlistdata[i].actPhotoAlbumNumber + '</div></div>' +
+							'<div class="alright_con"><h1>' + actlistdata[i].title + '</h1><p>' + actlistdata[i].act_date + '</p><p>' + actlistdata[i].venueTitle + '</p>' +
+							'<div class="al_km">' + actlistdata[i].z / 1000 + 'km</div>	</div></a></li>';
+					};
+					$(".Loading"+index).before(html);
+					scrollpage(index);
+				}
+			});
+		})
 
 
 
