fafacfe56e0abc64535c291c17402c07185cd4d2
wangyonglei <549749526@qq.com>
2016-08-15 22:24:08 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/photodetail.js
+++ js/photodetail.js
@@ -1,4 +1,47 @@
 $(document).ready(function() {
+
+	var ex_code = getCookie("ex_code");
+	var ex_mobile = getCookie("ex_mobile");
+	var ex_uid = getCookie("ex_uid");
+	var createTeam = function() {
+		
+	}
+	if (!ex_code && !ex_mobile && !ex_uid) {
+		var code = decodeURIComponent((new RegExp('[?|&]code=' + '([^&;]+?)(&|#|;|$)', "ig").exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
+		var locationUrl = window.location.href;
+		var ua = navigator.userAgent.toLowerCase();
+		if (ua.match(/MicroMessenger/i) == "micromessenger") {
+			if (!code) {
+				location = 'http://v.jgsports.com.cn/user/Act/getCode?backUri=' + locationUrl;
+			} else {
+				$.ajax({
+						url: 'http://v.jgsports.com.cn/user/User/login',
+						type: 'get',
+						dataType: 'json',
+						data: {
+							code: code
+						}
+					})
+					.done(function(data) {
+						photodetail()
+					})
+					.fail(function() {
+						console.log("error");
+					})
+					.always(function() {
+						console.log("complete");
+					});
+			}
+		} else {
+			alert('请在微信客户端打开！')
+		}
+	} else {
+		photodetail()
+	}
+
+
+ function photodetail(){
+
 	(function($) {
 		$.getUrlParam = function(name) {
 			var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
@@ -102,4 +145,5 @@ $(document).ready(function() {
 	$('#photo').on('click', '.dianzan', function() {
 		like();
 	})
+	}
 })
