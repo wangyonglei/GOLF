ef27be3eabe2b76977c759db20b2183d617cc571
wangyonglei <549749526@qq.com>
2016-08-15 23:59:48 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

---
+++ js/baoming.js
@@ -0,0 +1,67 @@
+$(document).ready(function($) {
+	var ex_code = getCookie("ex_code");
+	var ex_mobile = getCookie("ex_mobile");
+	var ex_uid = getCookie("ex_uid");
+
+	if (!ex_code && !ex_mobile && !ex_uid) {
+		var code = decodeURIComponent((new RegExp('[?|&]code=' + '([^&;]+?)(&|#|;|$)', "ig").exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
+		var locationUrl = window.location.href;
+		var ua = navigator.userAgent.toLowerCase();
+		if (ua.match(/MicroMessenger/i) == "micromessenger") {
+			if (!code) {
+				location = 'http://v.jgsports.com.cn/user/Act/getCode?backUri=' + locationUrl;
+			} else {
+				$.ajax({
+						url: 'http://v.jgsports.com.cn/user/User/login',
+						type: 'get',
+						dataType: 'json',
+						data: {
+							code: code
+						}
+					})
+					.done(function(data) {
+						baoming()
+					})
+					.fail(function() {
+						console.log("error");
+					})
+					.always(function() {
+						console.log("complete");
+					});
+			}
+		} else {
+			alert('请在微信客户端打开！')
+		}
+	} else {
+		baoming()
+	}
+
+
+	function baoming(){
+		$.ajax({
+				url: 'http://v.jgsports.com.cn/user/Act/getJoinActMembers ',
+				type: 'get',
+				dataType: 'json',
+				data: {
+					code: code,
+					act_id:id
+				}
+			})
+			.done(function(data) {
+				 var baomingdata = data.data;
+				  var html = '';
+				  
+			})
+			.fail(function() {
+				console.log("error");
+			})
+			.always(function() {
+				console.log("complete");
+			});
+	}
+	
+
+
+
+
+})
