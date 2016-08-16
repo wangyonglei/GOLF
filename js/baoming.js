fcfe4c8248d75447b99e54b51e8b7291f1de9919
wangyonglei <549749526@qq.com>
2016-08-16 10:57:38 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/baoming.js
+++ js/baoming.js
@@ -2,7 +2,6 @@ $(document).ready(function($) {
 	var ex_code = getCookie("ex_code");
 	var ex_mobile = getCookie("ex_mobile");
 	var ex_uid = getCookie("ex_uid");
-
 	if (!ex_code && !ex_mobile && !ex_uid) {
 		var code = decodeURIComponent((new RegExp('[?|&]code=' + '([^&;]+?)(&|#|;|$)', "ig").exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
 		var locationUrl = window.location.href;
@@ -20,7 +19,8 @@ $(document).ready(function($) {
 						}
 					})
 					.done(function(data) {
-						baoming()
+						baoming();
+						zengjia()
 					})
 					.fail(function() {
 						console.log("error");
@@ -33,42 +33,33 @@ $(document).ready(function($) {
 			alert('请在微信客户端打开！')
 		}
 	} else {
-		baoming()
+		baoming();
+		zengjia()
 	}
-
-
-	function baoming(){
+	function baoming() {
 		var id = decodeURIComponent((new RegExp('[?|&]id=' + '([^&;]+?)(&|#|;|$)', "ig").exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
-
 		$.ajax({
 				url: 'http://v.jgsports.com.cn/user/Act/getJoinActMembers',
 				type: 'get',
 				dataType: 'json',
 				data: {
 					code: code,
-					act_id:id
+					act_id: id
 				}
 			})
 			.done(function(data) {
-				 var baomingdata = data.data;
-				  var html = '';
-				  for (var i = 0; i <baomingdata.joinUserList.length; i++) {
-				  	html +='<li><div class="people_img"><img src="'+baomingdata.joinUserList[i].avatar+'"></div><p>'+baomingdata.joinUserList[i].rname+'</p></li>'
-				  }
-				  $('.yibaoming_people ul').html(html);
-
-				  var htmlwei = '';
-				  for (var i = 0; i <baomingdata.noJoinUserList.length; i++) {
-				  	htmlwei +='<li><div class="people_img"><img src="'+baomingdata.noJoinUserList[i].avatar+'"></div><p>'+baomingdata.noJoinUserList[i].rname+'</p></li>'
-				  }
-				  htmlwei +='<div class="jiajian">	<div class="people_img"><img src="images/jia.png"></div></div>'
-				  $('.weibaoming_people ul').html(htmlwei);
-
-
-
-
-
-
+				var baomingdata = data.data;
+				var html = '';
+				for (var i = 0; i < baomingdata.joinUserList.length; i++) {
+					html += '<li><div class="people_img"><img src="' + baomingdata.joinUserList[i].avatar + '"></div><p>' + baomingdata.joinUserList[i].rname + '</p></li>'
+				}
+				$('.yibaoming_people ul').html(html);
+				var htmlwei = '';
+				for (var i = 0; i < baomingdata.noJoinUserList.length; i++) {
+					htmlwei += '<li><div class="people_img"><img src="' + baomingdata.noJoinUserList[i].avatar + '"></div><p>' + baomingdata.noJoinUserList[i].rname + '</p></li>'
+				}
+				htmlwei += '<div class="jiajian">	<div class="people_img"><img src="images/jia.png"></div></div>'
+				$('.weibaoming_people ul').html(htmlwei);
 			})
 			.fail(function() {
 				console.log("error");
@@ -77,9 +68,27 @@ $(document).ready(function($) {
 				console.log("complete");
 			});
 	}
-	
-
-
-
-
+	function zengjia() {
+		$('.jiajian').click(function(event) {
+			$.ajax({
+					url: 'http://v.jgsports.com.cn/user/Act/inviteUserJoinAct',
+					type: 'post',
+					dataType: 'json',
+					data: {
+						code: code,
+						act_id: id,
+						joinMember: uid
+					}
+				})
+				.done(function() {
+					baoming();
+				})
+				.fail(function() {
+					console.log("error");
+				})
+				.always(function() {
+					console.log("complete");
+				});
+		});
+	}
 })
