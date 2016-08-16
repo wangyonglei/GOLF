a2c987385c674377d702195d648fd250c12f778e
wangyonglei <549749526@qq.com>
2016-08-16 17:26:21 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/jifenka.js
+++ js/jifenka.js
@@ -40,6 +40,23 @@ $(document).ready(function($) {
 
 
 	function jiFen() {
+		$.ajax({
+				url: 'http://v.jgsports.com.cn/user/Act/getUserIntegralCardInfoDetail',
+				type: 'get',
+				dataType: 'json',
+			})
+			.done(function(data) {
+				var xianshidata = dta.data;
+				var html = '';
+				html += '<div class="jifenka_name">大鹏</div><div>净杆<p class="jinggan">0</p></div><div>差点<p class="chadian">0</p></div>	<div>总杆<p class="zonggan">0</p></div>';
+				$('.top').html(html)
+			})
+			.fail(function() {
+				console.log("error");
+			})
+			.always(function() {
+				console.log("complete");
+			});
 
 
 
@@ -48,46 +65,47 @@ $(document).ready(function($) {
 			html += '<li><div class="list_num">' + j + '<p class="par">par5</p></div><div class="add">+</div><div class="reduce">-</div><div class="list_core"><input type="" name="" class="jifen_input" value="0"><p class="xintianweng">信天翁</p></div></li>'
 			$('.jifenka_li').append(html);
 		}
-		var uid = decodeURIComponent((new RegExp('[?|&]uid=' + '([^&;]+?)(&|#|;|$)', "ig").exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
 		var act_id = decodeURIComponent((new RegExp('[?|&]act_id=' + '([^&;]+?)(&|#|;|$)', "ig").exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
-
-		function getTotal() {
-			var jifenkadata = {
-				// uid: 3,
-				act_id: act_id,
-				total_bar: parseInt($('.zonggan').html()),
-				netBar: parseInt($('.jinggan').html()),
-				handicap: parseInt($('.chadian').html()),
-				one_hole: $('.jifenka_li li .jifen_input').eq(0).val(),
-				two_hole: $('.jifenka_li li .jifen_input').eq(1).val(),
-				three_hole: $('.jifenka_li li .jifen_input').eq(2).val(),
-				four_hole: $('.jifenka_li li .jifen_input').eq(3).val(),
-				five_hole: $('.jifenka_li li .jifen_input').eq(4).val(),
-				six_hole: $('.jifenka_li li .jifen_input').eq(5).val(),
-				seven_hole: $('.jifenka_li li .jifen_input').eq(6).val(),
-				eight_hole: $('.jifenka_li li .jifen_input').eq(7).val(),
-				nine_hole: $('.jifenka_li li .jifen_input').eq(8).val(),
-				ten_hole: $('.jifenka_li li .jifen_input').eq(9).val(),
-				eleven_hole: $('.jifenka_li li .jifen_input').eq(10).val(),
-				twelve_hole: $('.jifenka_li li .jifen_input').eq(11).val(),
-				thirteen_hole: $('.jifenka_li li .jifen_input').eq(12).val(),
-				fourteen_hole: $('.jifenka_li li .jifen_input').eq(13).val(),
-				fifteen_hole: $('.jifenka_li li .jifen_input').eq(14).val(),
-				sixteen_hole: $('.jifenka_li li .jifen_input').eq(15).val(),
-				seventeen_hole: $('.jifenka_li li .jifen_input').eq(16).val(),
-				eighteen_hole: $('.jifenka_li li .jifen_input').eq(17).val()
-			}
-			$.ajax({
-				url: 'http://v.jgsports.com.cn/user/Act/addUserIntegralCardInfo',
-				type: 'post',
-				dataType: 'json',
-				data: jifenkadata,
-				success: function(data) {
-					console.log('ok')
-				}
-			})
-		}
 		getTotal();
+
+
+	}
+
+	function getTotal() {
+		var jifenkadata = {
+			act_id: act_id,
+			total_bar: parseInt($('.zonggan').html()),
+			netBar: parseInt($('.jinggan').html()),
+			handicap: parseInt($('.chadian').html()),
+			one_hole: $('.jifenka_li li .jifen_input').eq(0).val(),
+			two_hole: $('.jifenka_li li .jifen_input').eq(1).val(),
+			three_hole: $('.jifenka_li li .jifen_input').eq(2).val(),
+			four_hole: $('.jifenka_li li .jifen_input').eq(3).val(),
+			five_hole: $('.jifenka_li li .jifen_input').eq(4).val(),
+			six_hole: $('.jifenka_li li .jifen_input').eq(5).val(),
+			seven_hole: $('.jifenka_li li .jifen_input').eq(6).val(),
+			eight_hole: $('.jifenka_li li .jifen_input').eq(7).val(),
+			nine_hole: $('.jifenka_li li .jifen_input').eq(8).val(),
+			ten_hole: $('.jifenka_li li .jifen_input').eq(9).val(),
+			eleven_hole: $('.jifenka_li li .jifen_input').eq(10).val(),
+			twelve_hole: $('.jifenka_li li .jifen_input').eq(11).val(),
+			thirteen_hole: $('.jifenka_li li .jifen_input').eq(12).val(),
+			fourteen_hole: $('.jifenka_li li .jifen_input').eq(13).val(),
+			fifteen_hole: $('.jifenka_li li .jifen_input').eq(14).val(),
+			sixteen_hole: $('.jifenka_li li .jifen_input').eq(15).val(),
+			seventeen_hole: $('.jifenka_li li .jifen_input').eq(16).val(),
+			eighteen_hole: $('.jifenka_li li .jifen_input').eq(17).val()
+		}
+		$.ajax({
+			url: 'http://v.jgsports.com.cn/user/Act/addUserIntegralCardInfo',
+			type: 'post',
+			dataType: 'json',
+			data: jifenkadata,
+			success: function(data) {
+				console.log('ok')
+			}
+		})
+
 		var jifenli = $('.jifenka_li li')
 			//为每行元素添加事件
 		for (var i = 0; i < jifenli.length; i++) {
