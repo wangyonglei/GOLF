aacab287b05a00dc1599cef7f2b00d50edc04cd0
wangyonglei <549749526@qq.com>
2016-08-16 20:54:15 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/activitydetail.js
+++ js/activitydetail.js
@@ -98,7 +98,7 @@ $(document).ready(function() {
 					
 					var jifenhtml = ''
 					for (var i = 0; i < act_det.userIntegralCardList.length; i++) {
-						jifenhtml += '<li><div class="chengjiicon"><img src="'+act_det.userIntegralCardList[i].avatar+'"></div><div class="chengjiname">'+act_det.userIntegralCardList[i].rname+'</div><p>'+act_det.userIntegralCardList[i].handicap+'</p><p>'+act_det.userIntegralCardList[i].holeNumber+'</p></li>'
+						jifenhtml += '<li><a href="jifen.html?id='+act_det.userIntegralCardList[i].id+'&uid='+act_det.userIntegralCardList[i].uid+'"><div class="chengjiicon"><img src="'+act_det.userIntegralCardList[i].avatar+'"></div><div class="chengjiname">'+act_det.userIntegralCardList[i].rname+'</div><p>'+act_det.userIntegralCardList[i].handicap+'</p><p>'+act_det.userIntegralCardList[i].holeNumber+'</p></a></li>'
 					}
 					$('.chengjicon').html(jifenhtml)
 
