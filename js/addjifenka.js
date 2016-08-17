ae2db72959c3cf131ba1284a2aa50c6c438b694d
wangyonglei <549749526@qq.com>
2016-08-17 16:08:54 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/addjifenka.js
+++ js/addjifenka.js
@@ -60,40 +60,37 @@ $(document).ready(function($) {
 			'<li><div class="list_num">18<p class="par">par5</p></div><div class="add">+</div><div class="reduce">-</div>	<div class="list_core">	<input type="" name="" class="jifen_input" value="0"><p class="xintianweng">信天翁</p>	</div></li>'
 		$('.jifenka_li').html(lihtml);
 		//修改请求
+	
+
+
 		function getTotal() {
-			var jifenkadata = {
-				total_bar: parseInt($('.zonggan').html()),
-				netBar: parseInt($('.jinggan').html()),
-				handicap: parseInt($('.chadian').html()),
-				one_hole: $('.jifenka_li li .jifen_input').eq(0).val(),
-				two_hole: $('.jifenka_li li .jifen_input').eq(1).val(),
-				three_hole: $('.jifenka_li li .jifen_input').eq(2).val(),
-				four_hole: $('.jifenka_li li .jifen_input').eq(3).val(),
-				five_hole: $('.jifenka_li li .jifen_input').eq(4).val(),
-				six_hole: $('.jifenka_li li .jifen_input').eq(5).val(),
-				seven_hole: $('.jifenka_li li .jifen_input').eq(6).val(),
-				eight_hole: $('.jifenka_li li .jifen_input').eq(7).val(),
-				nine_hole: $('.jifenka_li li .jifen_input').eq(8).val(),
-				ten_hole: $('.jifenka_li li .jifen_input').eq(9).val(),
-				eleven_hole: $('.jifenka_li li .jifen_input').eq(10).val(),
-				twelve_hole: $('.jifenka_li li .jifen_input').eq(11).val(),
-				thirteen_hole: $('.jifenka_li li .jifen_input').eq(12).val(),
-				fourteen_hole: $('.jifenka_li li .jifen_input').eq(13).val(),
-				fifteen_hole: $('.jifenka_li li .jifen_input').eq(14).val(),
-				sixteen_hole: $('.jifenka_li li .jifen_input').eq(15).val(),
-				seventeen_hole: $('.jifenka_li li .jifen_input').eq(16).val(),
-				eighteen_hole: $('.jifenka_li li .jifen_input').eq(17).val()
+			var zonggan = parseInt($('.zonggan').html());
+			var jinggan = parseInt($('.jinggan').html());
+			var chadian = parseInt($('.chadian').html());
+
+
+			var one_hole = $('.jifenka_li li .jifen_input').eq(0).val();
+			var two_hole= $('.jifenka_li li .jifen_input').eq(1).val();
+			var three_hole= $('.jifenka_li li .jifen_input').eq(2).val();
+			var four_hole= $('.jifenka_li li .jifen_input').eq(3).val();
+			var five_hole= $('.jifenka_li li .jifen_input').eq(4).val();
+			var six_hole= $('.jifenka_li li .jifen_input').eq(5).val();
+			var seven_hole= $('.jifenka_li li .jifen_input').eq(6).val();
+			var eight_hole= $('.jifenka_li li .jifen_input').eq(7).val();
+			var nine_hole= $('.jifenka_li li .jifen_input').eq(8).val();
+			var ten_hole= $('.jifenka_li li .jifen_input').eq(9).val();
+			var eleven_hole=$('.jifenka_li li .jifen_input').eq(10).val();
+			var twelve_hole=$('.jifenka_li li .jifen_input').eq(11).val();
+			var thirteen_hole= $('.jifenka_li li .jifen_input').eq(12).val();
+			var fourteen_hole= $('.jifenka_li li .jifen_input').eq(13).val();
+			var fifteen_hole= $('.jifenka_li li .jifen_input').eq(14).val();
+			var sixteen_hole= $('.jifenka_li li .jifen_input').eq(15).val();
+			var seventeen_hole= $('.jifenka_li li .jifen_input').eq(16).val();
+			var eighteen_hole= $('.jifenka_li li .jifen_input').eq(17).val();
+
+			zonggan = one_hole + two_hole+three_hole+four_hole+five_hole+six_hole+seven_hole+eight_hole+nine_hole+ten_hole+eleven_hole+twelve_hole+thirteen_hole+fourteen_hole+fifteen_hole+sixteen_hole+seventeen_hole+eighteen_hole
 			}
-			$.ajax({
-				url: 'http://v.jgsports.com.cn/user/Act/addUserIntegralCardInfo',
-				type: 'post',
-				dataType: 'json',
-				data: jifenkadata,
-				success: function(data) {
-					
-				}
-			})
-		}
+
 		//编辑操作
 		function jiajianxiugai() {
 			var jifenli = $('.jifenka_li li')
@@ -109,14 +106,17 @@ $(document).ready(function($) {
 						//通过判断触发元素的class确定用户点击了哪个元素
 						switch (cls) {
 							case 'add': //点击了加号
+								if (value < 50) {
 								countInout.value = value + 1;
 								getTotal();
+								}
 								break;
+
 							case 'reduce': //点击了减号
-								// if (value > 1) {
+								if (value > 1) {
 								countInout.value = value - 1;
 								getTotal();
-								// }
+								}
 								break;
 						}
 						// getTotal();
@@ -135,5 +135,45 @@ $(document).ready(function($) {
 			}
 		}
 		jiajianxiugai();
+
+
+		$('.tijiao_tbn').click(function(event) {
+			
+				var jifenkadata = {
+					total_bar: parseInt($('.zonggan').html()),
+					netBar: parseInt($('.jinggan').html()),
+					handicap: parseInt($('.chadian').html()),
+					one_hole: $('.jifenka_li li .jifen_input').eq(0).val(),
+					two_hole: $('.jifenka_li li .jifen_input').eq(1).val(),
+					three_hole: $('.jifenka_li li .jifen_input').eq(2).val(),
+					four_hole: $('.jifenka_li li .jifen_input').eq(3).val(),
+					five_hole: $('.jifenka_li li .jifen_input').eq(4).val(),
+					six_hole: $('.jifenka_li li .jifen_input').eq(5).val(),
+					seven_hole: $('.jifenka_li li .jifen_input').eq(6).val(),
+					eight_hole: $('.jifenka_li li .jifen_input').eq(7).val(),
+					nine_hole: $('.jifenka_li li .jifen_input').eq(8).val(),
+					ten_hole: $('.jifenka_li li .jifen_input').eq(9).val(),
+					eleven_hole: $('.jifenka_li li .jifen_input').eq(10).val(),
+					twelve_hole: $('.jifenka_li li .jifen_input').eq(11).val(),
+					thirteen_hole: $('.jifenka_li li .jifen_input').eq(12).val(),
+					fourteen_hole: $('.jifenka_li li .jifen_input').eq(13).val(),
+					fifteen_hole: $('.jifenka_li li .jifen_input').eq(14).val(),
+					sixteen_hole: $('.jifenka_li li .jifen_input').eq(15).val(),
+					seventeen_hole: $('.jifenka_li li .jifen_input').eq(16).val(),
+					eighteen_hole: $('.jifenka_li li .jifen_input').eq(17).val()
+				}
+				$.ajax({
+					url: 'http://v.jgsports.com.cn/user/Act/addUserIntegralCardInfo',
+					type: 'post',
+					dataType: 'json',
+					data: jifenkadata,
+					success: function(data) {
+						console('ok')
+					}
+				})
+
+
+
+		});
 	}
 })
