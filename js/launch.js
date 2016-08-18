f1deaed7b088e607419ec00d9e106be1991bce2b
wangyonglei <549749526@qq.com>
2016-08-18 14:35:25 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/launch.js
+++ js/launch.js
@@ -68,36 +68,38 @@ $(document).ready(function($) {
 				$('#search_input').val('');
 			})
 			// 邀请好友
-		$('.yaoqing').click(function() {
-			$('.yaoqing_maxk').css('display', 'block');
-			// $('.yaoqing_yq').html('')
-		})
-		var htmlrname =''
-		$('.yaoqing_btn').click(function(event) {
-			var rname = $('.yaoqing_name').val();
-			var mobile = $('.yaoqing_tel').val();
-			$('.yaoqing_span').css('display','none');
-			htmlrname += ''+rname+',';
-			$('.yaoqing_yq').append(htmlrname);
-			var yaoqing_namrel = {
-				rname: $('.yaoqing_name').val(),
-				mobile: $('.yaoqing_tel').val()
-			}
-			$('.yaoqing_maxk').css('display', 'none');
-			$.ajax({
-					url: 'http://v.jgsports.com.cn/user/User/addUserInfo',
-					type: 'post',
-					dataType: 'json',
-					data: yaoqing_namrel
-				})
-				.done(function(data) {
-					var data = data.data;
-					console.log('O(∩_∩)O哈哈~')
-					$('.yaoqing_name').val('');
-					$('.yaoqing_tel').val('');
-					$('.datadata').append(data+',')
-				})
-		});
+		// $('.yaoqing').click(function() {
+		// 	$('.yaoqing_maxk').css('display', 'block');
+		// 	// $('.yaoqing_yq').html('')
+		// })
+
+		
+		// var htmlrname =''
+		// $('.yaoqing_btn').click(function(event) {
+		// 	var rname = $('.yaoqing_name').val();
+		// 	var mobile = $('.yaoqing_tel').val();
+		// 	$('.yaoqing_span').css('display','none');
+		// 	htmlrname += ''+rname+',';
+		// 	$('.yaoqing_yq').append(htmlrname);
+		// 	var yaoqing_namrel = {
+		// 		rname: $('.yaoqing_name').val(),
+		// 		mobile: $('.yaoqing_tel').val()
+		// 	}
+		// 	$('.yaoqing_maxk').css('display', 'none');
+		// 	$.ajax({
+		// 			url: 'http://v.jgsports.com.cn/user/User/addUserInfo',
+		// 			type: 'post',
+		// 			dataType: 'json',
+		// 			data: yaoqing_namrel
+		// 		})
+		// 		.done(function(data) {
+		// 			var data = data.data;
+		// 			console.log('O(∩_∩)O哈哈~')
+		// 			$('.yaoqing_name').val('');
+		// 			$('.yaoqing_tel').val('');
+		// 			$('.datadata').append(data+',')
+		// 		})
+		// });
 
 		// 通讯录icon
 		var tongxunlu = function(){
@@ -176,33 +178,6 @@ $(document).ready(function($) {
 
 
 
-
-
-		// var htmlrname =''
-		// $('.yaoqing_btn').click(function(event) {
-		// 	var rname = $('.yaoqing_name').val();
-		// 	var mobile = $('.yaoqing_tel').val();
-		// 	htmlrname += ''+rname+',';
-		// 	$('.yaoqing_yq').val(htmlrname);
-		// 	var yaoqing_namrel = {
-		// 		rname: $('.yaoqing_name').val(),
-		// 		mobile: $('.yaoqing_tel').val()
-		// 	}
-		// 	$('.yaoqing_maxk').css('display', 'none');
-		// 	$.ajax({
-		// 			url: 'http://v.jgsports.com.cn/user/User/addUserInfo',
-		// 			type: 'post',
-		// 			dataType: 'json',
-		// 			data: yaoqing_namrel
-		// 		})
-		// 		.done(function(data) {
-		// 			var data = data.data;
-		// 			console.log('O(∩_∩)O哈哈~')
-		// 			$('.yaoqing_name').val('');
-		// 			$('.yaoqing_tel').val('');
-		// 			$('.datadata').html(data)
-		// 		})
-		// });
 
 
 
