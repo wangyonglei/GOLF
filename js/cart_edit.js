401cf8f2470ca8b3a5a2ee46946eb923051b8308
wangyonglei <549749526@qq.com>
2016-09-04 21:37:52 +0800

    Signed-off-by: wangyonglei <549749526@qq.com>

--- js/cart_edit.js
+++ js/cart_edit.js
@@ -100,7 +100,7 @@ $(document).ready(function($) {
 
 
 
-	$('body').on('click', '.baocun', function() {
+	$('document.body').on( '.baocun', 'click', function() {
 		alert('保存')
 		var datacart = {
 			signature: $('.signature').val(),
